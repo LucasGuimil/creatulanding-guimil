e0e59e36ccc2e43dd53b3e5dcf7dd46396aa9053
LucasGuimil <l.d.guimil@gmail.com>
2025-03-23 16:33:19 -0300

    creando carrito de compras

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CartWidget() {
+  return (
+    <div className='btn btn-dark'>
+      <a><img className= "cart" src="../src/assets/shopping-cart.png" alt="" /></a>
+    </div>
+  )
+}
+
+export default CartWidget
