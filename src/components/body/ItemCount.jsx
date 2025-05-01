9a6984ff0f6856f0d9e911e2235152e05fcf5bb0
LucasGuimil <l.d.guimil@gmail.com>
2025-05-01 12:16:08 -0300

    corrección de boton sumar y restar

--- src/components/body/ItemCount.jsx
+++ src/components/body/ItemCount.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 
 const ItemCount = ({stock}) => {
     const [count, setCount] = useState(0)
-    const sum = ()=>{count===stock ? setCount(stock) : setCount(count + 1)}
-    const rest = ()=>{count===0 ? setCount(0) : setCount(count - 1)}
+    const sum = ()=>{setCount(count + 1)}
+    const rest = ()=>{setCount(count - 1)}
 
     return (
         <div className='btn-group align-items-center'>
-            <button className='btn-dark' onClick={rest}>-</button>
+            <button className='btn btn-dark' onClick={rest} disabled={count==0}>-</button>
             <h4 className='mx-2'>{count}</h4>
-            <button className='btn-dark' onClick={sum}>+</button>  
+            <button className='btn btn-dark' onClick={sum} disabled={count>=stock}>+</button>  
         </div>
     )
 }   
