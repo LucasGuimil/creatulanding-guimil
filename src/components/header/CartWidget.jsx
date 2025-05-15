7e97a9f361bc10a607791b74269e8eedf615bc4b
LucasGuimil <l.d.guimil@gmail.com>
2025-05-15 18:04:37 -0300

    corrección de enlaces de imágenes

--- src/components/header/CartWidget.jsx
+++ src/components/header/CartWidget.jsx
@@ -7,7 +7,7 @@ function CartWidget() {
   return (
     <button className="btn fondo-oscuro">
       <div className="position-relative">
-        <Link to="/cart"><img className= "cart" src="/public/assets/images/shopping-cart.svg" alt="" height="40px"/></Link>
+        <Link to="/cart"><img className= "cart" src="/assets/images/shopping-cart.svg" alt="" height="40px"/></Link>
         <span hidden={cartQuantity===0?true:false}className="position-absolute top-0 start-100 translate-middle badge rounded-circle bg-danger p-1"><span>{cartQuantity}</span></span>
       </div>
     </button>
