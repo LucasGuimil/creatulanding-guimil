d928de4a86bbd2755575197891b3b27352f45631
LucasGuimil <l.d.guimil@gmail.com>
2025-05-15 21:19:27 -0300

    corrigiendo detalles del diseño de itemList y navBar

--- src/components/body/ItemList.jsx
+++ src/components/body/ItemList.jsx
@@ -3,7 +3,7 @@ import Item from './Item'
 
 const ItemList = ({ list }) => {
   return (
-    <div className='row row-cols-3 g-4'>
+    <div className='row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4'>
       {list.map(product => (
         <Item product={product} key={product.id}/>
       ))}
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router'
 
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-sm mt-0 mb-2 navbar-dark fondo-oscuro">
+        <nav className="navbar navbar-expand-md mt-0 mb-2 navbar-dark fondo-oscuro">
             <div className="container-fluid justify-content-between">
                 <Link className="navbar-brand p-2" to="/"><img src="/assets/images/av-logo.png" width="100px" /></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,9 +25,9 @@ function NavBar() {
                             <NavLink className={({isActive})=> isActive?'active nav-link fw-bold fs-4':'nav-link fw-bold fs-4'} to="/category/Accesorios" end>Accesorios</NavLink>
                         </li>
                     </ul>
-                </div>
                 <div className="p-4">
                     <CartWidget />
+                </div>
                 </div>
             </div>
         </nav>
